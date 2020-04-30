26fcab966e63d94e6e68c14c3378772f1787aeac
Hugo Galvão <hugo.galvao@thoughtworks.com>
2020-04-30 15:55:37 -0300

    Adding deposit and withdraw endpoints

--- simple-bank-api/src/controllers/accountController.ts
+++ simple-bank-api/src/controllers/accountController.ts
@@ -2,8 +2,33 @@ import { getRepository } from "typeorm";
 import { Account } from "../entity/Account";
 
 export const balance = async (req, res) => {
+    const id = req.params.id;
+    const balance = await getRepository(Account).findOne(id);
+    
+    console.log(balance);
+    res.status(200).send(balance);
+}
+
+export const deposit = async (req, res) => {
+    const id = req.params.id;
+    const depositValue = req.body.value;
+
     const accountRepository = getRepository(Account);
-    const accounts = await accountRepository.find();
-    console.log(accounts);
-    res.send(accounts);
+    await accountRepository.increment({id: id}, "balance", depositValue);
+    const balance = await accountRepository.findOne(id);
+
+    console.log(balance);
+    res.status(200).send(balance);
+}
+
+export const withdraw = async (req, res) => {
+    const id = req.params.id;
+    const depositValue = req.body.value;
+
+    const accountRepository = getRepository(Account);
+    await accountRepository.decrement({id: id}, "balance", depositValue);
+    const balance = await accountRepository.findOne(id);
+
+    console.log(balance);
+    res.status(200).send(balance);
 }
--- simple-bank-api/src/routes/account.ts
+++ simple-bank-api/src/routes/account.ts
@@ -3,6 +3,10 @@ import * as accountController from '../controllers/accountController'
 
 const router = express.Router();
 
-router.get('/balance', accountController.balance);
+router.get('/balance/:id', accountController.balance);
+
+router.put('/deposit/:id', accountController.deposit);
+
+router.put('/withdraw/:id', accountController.withdraw);
 
 export default router;
