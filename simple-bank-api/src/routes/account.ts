69f7dd17440ebd7a687a7c9e8dc98dfe6d34a402
Hugo Galvão <hugo.galvao@thoughtworks.com>
2020-05-02 13:59:19 -0300

    Make endpoints more restful

--- simple-bank-api/src/routes/account.ts
+++ simple-bank-api/src/routes/account.ts
@@ -3,17 +3,17 @@ import * as accountController from "../controllers/accountController";
 
 const router = express.Router();
 
-router.get(":id/balance", accountController.balance);
+router.get("/:id/balance", accountController.balance);
 
 router.put(
-  ":id/deposit",
+  "/:id/deposit",
   accountController.validators,
   accountController.validate,
   accountController.deposit
 );
 
 router.put(
-  ":id/withdraw",
+  "/:id/withdraw",
   accountController.validators,
   accountController.validate,
   accountController.withdraw
