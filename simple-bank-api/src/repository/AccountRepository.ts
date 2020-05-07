258df4107644194666e790dad517273ebd9f091a
Hugo Galvão <hugo.galvao@thoughtworks.com>
2020-05-07 15:20:30 -0300

    Refactoring AccountRepository methods

--- simple-bank-api/src/repository/AccountRepository.ts
+++ simple-bank-api/src/repository/AccountRepository.ts
@@ -3,33 +3,20 @@ import { Account } from "../entity/Account";
 
 @EntityRepository(Account)
 export class AccountRepository extends Repository<Account> {
-  deposit(account: Account, value: number): Promise<Account> {
-    return new Promise((resolve, reject) => {
-      account.balance += Number(value);
-      this.save(account)
-        .then((result) => {
-          resolve(result);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+  async deposit(account: Account, value: number): Promise<Account> {
+    account.balance += Number(value);
+
+    return await this.save(account);
   }
 
-  withdraw(account: Account, value: number): Promise<Account> {
-    return new Promise((resolve, reject) => {
-      if (this.isValueEqualOrLessThanBalance(account.balance, value)) {
-        return reject("Balance not enough to withdraw this value.")
-      }
-      account.balance -= Number(value);
-      this.save(account)
-        .then((result) => {
-          resolve(result);
-        })
-        .catch((error) => {
-          reject(error);
-        });
-    });
+  async withdraw(account: Account, value: number): Promise<Account> {
+    if (this.isValueEqualOrLessThanBalance(account.balance, value)) {
+      throw new Error("Balance not enough to withdraw this value.");
+    }
+
+    account.balance -= Number(value);
+
+    return await this.save(account);
   }
 
   private isValueEqualOrLessThanBalance(balance, value) {
